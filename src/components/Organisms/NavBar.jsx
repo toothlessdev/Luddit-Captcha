5779e3811374b154f58fb335ef08b985896ad952
toothlessdev <toothless042@gmail.com>
2023-08-03 19:31:18 +0900

    init

---
+++ src/components/Organisms/NavBar.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export const NavBar = () => {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <Link href="/">Home</Link>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );
+};
---
+++ src/components/Templates/Header.jsx
@@ -0,0 +1,9 @@
+import { NavBar } from "../Organisms/NavBar";
+
+export const Header = () => {
+    return (
+        <>
+            <NavBar></NavBar>
+        </>
+    );
+};
