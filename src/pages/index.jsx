6d62f2e9be5f1deaf14182f723558437a62abe46
toothlessdev <toothless042@gmail.com>
2023-08-05 05:11:29 +0900

    feat: SEO 검색엔진 최적화

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import { AboutSection } from "@/components/Templates/AboutSection";
 import { HeroSection } from "@/components/Templates/HeroSection";
 import { PartenerSection } from "@/components/Templates/PartenerSection";
@@ -5,6 +7,23 @@ import { PartenerSection } from "@/components/Templates/PartenerSection";
 export default function HomePage() {
     return (
         <>
+            <Head>
+                <title>"Luddit Captcha</title>
+                <meta name="description" content="Captcha For Next Generation" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Luddit Captcha" />
+                <meta property="og:description" content="Captcha For Next Generation" />
+                <meta property="og:url" content="페이지 주소" />
+
+                <meta property="twitter:card" content="summary" />
+                <meta property="twitter:title" content="Luddit Captcha" />
+                <meta property="twitter:description" content="Captcha For Next Generation" />
+
+                <meta property="og:article:author" content="github.com/toothlessdev" />
+            </Head>
+
             <HeroSection />
             <AboutSection />
             <PartenerSection />
