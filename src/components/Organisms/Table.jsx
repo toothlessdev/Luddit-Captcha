e14c11dd9c0c8861ecd6595d4c7e6107f4400a10
toothlessdev <toothless042@gmail.com>
2023-08-05 00:47:51 +0900

    feat: Table 컴포넌트 구현완료

---
+++ src/components/Organisms/Table.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useRef } from "react";
+import style from "./Table.module.scss";
+
+export const Table = ({ children }) => {
+    return <div className={style.container}>{children}</div>;
+};
+
+export const TableHead = ({ children }) => {
+    return <div className={style.head}>{children}</div>;
+};
+
+export const TableBody = ({ children }) => {
+    return <div className={style.body}>{children}</div>;
+};
+
+export const TableGroup = ({ children }) => {
+    return (
+        <div className={style.group} itemSize={children.length}>
+            {children}
+        </div>
+    );
+};
+
+export const TableItem = ({ width, height, itemSize, children }) => {
+    const itemRef = useRef();
+
+    useEffect(() => {
+        itemRef.current.style.width = `${100 / itemSize}%`;
+    }, []);
+
+    return (
+        <div ref={itemRef} className={style.item} style={{ width: width, height: height }}>
+            {children}
+        </div>
+    );
+};
